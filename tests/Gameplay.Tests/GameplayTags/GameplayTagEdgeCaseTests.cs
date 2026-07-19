f0c056fb70a9a395d666ccb26a87e7eeef11aca4
Moozen <1365570355@qq.com>
2026-07-19 21:47:24 +0800

    test: GameplayTag 边界场景测试

---
+++ tests/Gameplay.Tests/GameplayTags/GameplayTagEdgeCaseTests.cs
@@ -0,0 +1,106 @@
+using System;
+using Friflo.Engine.ECS;
+using Xunit;
+
+namespace Gameplay.Tests;
+
+public class GameplayTagEdgeCaseTests
+{
+    [Fact]
+    public void TagName_Trimmed_BeforeRegistration()
+    {
+        GameplayTagManager.RegisterTags("  Damage  ");
+        var tag = GameplayTag.Request("Damage");
+        Assert.True(tag.IsValid);
+    }
+
+    [Fact]
+    public void TagName_Empty_Throws()
+    {
+        Assert.Throws<ArgumentException>(() =>
+            GameplayTagManager.RegisterTags(""));
+    }
+
+    [Fact]
+    public void TagName_StartsWithDot_Throws()
+    {
+        Assert.Throws<ArgumentException>(() =>
+            GameplayTagManager.RegisterTags(".Damage"));
+    }
+
+    [Fact]
+    public void TagName_EndsWithDot_Throws()
+    {
+        Assert.Throws<ArgumentException>(() =>
+            GameplayTagManager.RegisterTags("Damage."));
+    }
+
+    [Fact]
+    public void TagName_DoubleDot_Throws()
+    {
+        Assert.Throws<ArgumentException>(() =>
+            GameplayTagManager.RegisterTags("Damage..Fire"));
+    }
+
+    [Fact]
+    public void TagName_CaseSensitive()
+    {
+        GameplayTagManager.RegisterTags("Damage");
+        var upper = GameplayTag.Request("Damage");
+        var lower = GameplayTag.Request("damage");
+
+        Assert.True(upper.IsValid);
+        Assert.False(lower.IsValid); // 大小写敏感
+    }
+
+    [Fact]
+    public void ToString_ReturnsFullName()
+    {
+        GameplayTagManager.RegisterTags("Damage.Fire");
+        var fire = GameplayTag.Request("Damage.Fire");
+        Assert.Equal("Damage.Fire", fire.ToString());
+    }
+
+    [Fact]
+    public void OperateOnMultipleEntities_EachHasOwnTagSet()
+    {
+        GameplayTagManager.RegisterTags("Damage", "Buff");
+        var world = new World(NetMode.Standalone);
+
+        var entityA = world.Store.CreateEntity();
+        var entityB = world.Store.CreateEntity();
+
+        entityA.AddComponent(new GameplayTags());
+        entityB.AddComponent(new GameplayTags());
+
+        ref var tagsA = ref entityA.GetComponent<GameplayTags>();
+        ref var tagsB = ref entityB.GetComponent<GameplayTags>();
+
+        tagsA.AddTag(GameplayTag.Request("Damage"));
+        tagsB.AddTag(GameplayTag.Request("Buff"));
+
+        Assert.True(tagsA.HasTag(GameplayTag.Request("Damage")));
+        Assert.False(tagsA.HasTag(GameplayTag.Request("Buff")));
+        Assert.False(tagsB.HasTag(GameplayTag.Request("Damage")));
+        Assert.True(tagsB.HasTag(GameplayTag.Request("Buff")));
+    }
+
+    [Fact]
+    public void RegisterTags_Incremental_Works()
+    {
+        // 先注册一批
+        GameplayTagManager.RegisterTags("Damage");
+        var dmg1 = GameplayTag.Request("Damage");
+        Assert.True(dmg1.IsValid);
+
+        // 再注册新的一批（增量）
+        GameplayTagManager.RegisterTags("Damage.Fire", "Damage.Ice");
+        var fire = GameplayTag.Request("Damage.Fire");
+        var ice  = GameplayTag.Request("Damage.Ice");
+
+        Assert.True(fire.IsValid);
+        Assert.True(ice.IsValid);
+        Assert.True(fire.Matches(dmg1));
+        Assert.True(ice.Matches(dmg1));
+    }
+}
